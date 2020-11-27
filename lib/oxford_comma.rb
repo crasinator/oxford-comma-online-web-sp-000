b8d851b00e54a2d1f6add47511ef820653fb5dd1
Jordan Reed <jordanreed03@gmail.com>
2020-11-27 21:56:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
 def oxford_comma(array)
+  if array.count == 1
+    var = array.join
+      return var
+  elsif array.count == 2 
+    array.insert(1, " and ")
+    var = array.join
+      return var 
+  elsif array.count == 3 
+    array.insert(1, ", ")
+    array.insert(3, ", and ")
+    var = array.join
+      return var 
+      
+  elsif array.count > 3 
+  tricky = array.count - 1
+    array.insert(tricky, ", and ")
+    array.insert(1, ", ")
+    
+  tricky = array.count - 1
+  array.insert(1, ", ")
+  
+  count = 0 
+    var = array.join
+      return var 
+  end 
+  
+end 
 
-end
